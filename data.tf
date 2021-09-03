5930734f96434ed4f8934d25effdefd007d169e0
Paul.Hasse <Paul.Hasse@ngahr.com>
2021-09-03 17:03:38 +0200

    data

---
+++ data.tf
@@ -0,0 +1,10 @@
+data "terraform_remote_state" "rg" {
+    backend = "remote"
+    config {
+        hostname = "app.terraform.io"
+	organization = "example-org-aba90f"
+        workspaces {
+          prefix = "testRepo"
+        }
+    }
+}
